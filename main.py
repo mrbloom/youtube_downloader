5b0a9bba896a2a539a37c4033907bc0125bb40cc
mrbloom <mrbloom@ukr.net>
2023-09-25 02:34:35 +0300

    add gui

--- main.py
+++ main.py
@@ -1,55 +1,91 @@
 import tkinter as tk
-from tkinter import simpledialog, messagebox
-from pytube import YouTube
-from moviepy.editor import concatenate_audioclips, AudioFileClip, VideoFileClip
+from tkinter import simpledialog, messagebox, ttk
+from pytube import YouTube, Stream
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+
+def progress(stream: Stream, chunk: bytes, bytes_remaining: int):
+    current = stream.filesize - bytes_remaining
+    percentage = (current / stream.filesize) * 100
+    progress_bar["value"] = percentage
+    root.update_idletasks()
+
+    # Command line progress
+    print(f"\rProgress: {percentage:.2f}%", end="")
+
+
+def resolution_sort_key(resolution_string):
+    try:
+        return int(resolution_string.replace('p', ''))
+    except ValueError:
+        return 0
+
+
+def fetch_resolutions(event=None):  # Added default value for event
+    url = url_entry.get()
+    try:
+        resolutions = get_resolutions(url)
+        resolution_combobox["values"] = resolutions
+        if resolutions:
+            resolution_combobox.current(0)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+def get_resolutions(url):
+    yt = YouTube(url)
+    video_streams = yt.streams.filter(only_video=True, file_extension='mp4')
+    resolutions = list({stream.resolution for stream in video_streams})
+    resolutions.sort(key=resolution_sort_key)
+    return resolutions
 
 
 def download_and_combine():
+    global blinking
     url = url_entry.get()
+    selected_resolution = resolution_combobox.get()
 
     yt = YouTube(url)
+    yt.register_on_progress_callback(progress)
 
-    # Get the video stream with the highest resolution without audio codec
-    video_stream = yt.streams.filter(only_video=True, file_extension='mp4').order_by("resolution").desc().first()
-
-    # Get the best audio stream
+    video_stream = yt.streams.filter(only_video=True, resolution=selected_resolution, file_extension='mp4').first()
     audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
 
     if not video_stream or not audio_stream:
         messagebox.showerror("Error", "Suitable streams not found.")
         return
 
-    status_label.config(text=f"Downloading video: {yt.title} in {video_stream.resolution}")
+    # Downloading and combining code
     video_filename = video_stream.download(filename="video_temp")
-
-    status_label.config(text=f"Downloading audio for: {yt.title}")
     audio_filename = audio_stream.download(filename="audio_temp")
 
-    status_label.config(text="Combining video and audio...")
     video = VideoFileClip(video_filename)
     audio = AudioFileClip(audio_filename)
 
     final_clip = video.set_audio(audio)
     final_clip.write_videofile(f"{yt.title}.mp4")
 
-    status_label.config(text="Download and merge completed!")
-    messagebox.showinfo("Success", "Download and merge completed!")
+    print("\nDownload and merge completed!")
 
 
-# Create the main window
 root = tk.Tk()
 root.title("YouTube Downloader")
 
-# Add an Entry widget for the user to enter the URL
 url_entry = tk.Entry(root, width=50)
 url_entry.pack(pady=20)
 url_entry.insert(0, "Enter the YouTube video URL")
+url_entry.bind("<Button-1>", lambda e: url_entry.delete(0, tk.END))
+url_entry.bind("<Return>", fetch_resolutions)  # When Enter is pressed after typing the URL
 
-# Add a button to start the download process
+resolution_combobox = ttk.Combobox(root, state="readonly")
+resolution_combobox.pack(pady=10)
+
 download_button = tk.Button(root, text="Download", command=download_and_combine)
-download_button.pack(pady=20)
+download_button.pack(pady=10)
 
-# Add a label to show the status
+progress_bar = ttk.Progressbar(root, orient="horizontal", mode="determinate", length=400)
+progress_bar.pack(pady=20)
+
 status_label = tk.Label(root, text="", bd=1, relief="solid", anchor="w")
 status_label.pack(pady=20, fill=tk.X)
 
